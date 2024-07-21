153fd358c45692ed180dfcd924c567b80ac48ef9
luchitoide <luchitoiderp@gmail.com>
2024-07-21 13:45:11 -0500

    copiar funcionando

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+textarea = getElementById("texto")
+
+textarea.addEventListener('input', () => {
+  if (textarea.value === '') {
+    placeholderImage.style.display = 'block';
+  } 
+});
+
 function encriptarTexto() {
     const texto = document.getElementById('texto').value;
     let textoEncriptado = texto
@@ -8,6 +16,7 @@ function encriptarTexto() {
       .replace(/u/g, 'ufat');
   
     document.getElementById('resultado').textContent = textoEncriptado;
+    mostrarElementos();
   }
   
   function desencriptarTexto() {
@@ -20,4 +29,30 @@ function encriptarTexto() {
       .replace(/ufat/g, 'u');
   
     document.getElementById('resultado').textContent = textoDesencriptado;
+    mostrarElementos();
+  }
+
+  function mostrarElementos() {
+    document.getElementById("imagen").style.display = "none";
+    document.getElementById("boton-copiar").style.display = "block";
+  }
+
+  function ocultarElementos() {
+    document.getElementById("imagen").style.display = "block";
+    document.getElementById("boton-copiar").style.display = "none";
+  }
+
+  function copiarTexto(){
+    let contenido = document.getElementById("resultado").innerText;
+     var temporal = document.createElement("textarea");
+     temporal.value = contenido;
+     document.body.appendChild(temporal);
+
+     temporal.select();
+     temporal.setSelectionRange(0, 99999);
+
+     document.execCommand("copy");
+
+     document.body.removeChild(temporal);
+
   }
